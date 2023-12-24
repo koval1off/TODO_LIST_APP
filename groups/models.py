a06d3b84e8357b163eb1260ef5d405404ac64381
Paul <kovaliov043@gmail.com>
2023-12-24 12:47:33 +0200

    add uuid for groups and todos

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 
 class TaskGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- todos/models.py
+++ todos/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from groups.models import TaskGroup
+import uuid
 
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(TaskGroup, on_delete=models.CASCADE, related_name='tasks', null=True, default=None)
     title = models.CharField(max_length=200)
